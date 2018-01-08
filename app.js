a7b9edc0dc2e631ad52aae8d0dec53d2f4169672
pomfy113 <pomfy113@gmail.com>
2018-01-08 15:48:32 -0800

    Initial set-up

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+// Used for authentication
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken');
+// It's express
+const app = express()
+
+// Public folder
+app.use(express.static('public'))
+
+// View Engine - Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Database - Mongoose
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/WEB-3-Project', {useMongoClient: true});
+
+// // Authorization
+let checkAuth = (req, res, next) => {
+  // If there's a cookie, they should be logged in
+  if (typeof req.cookies.nToken === 'undefined' || req.cookies.nToken === null) {
+    req.user = null;
+  } else {
+    // Success! Decode the token, then put that payload into req.user
+    let token = req.cookies.nToken;
+    let decodedToken = jwt.decode(token, { complete: true }) || {};
+    req.user = decodedToken.payload;
+    console.log("User currently logged in", req.user.username)
+  }
+  next()
+}
+
+// // Run checkAuth
+app.use(checkAuth)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
---
+++ controllers/auth.js
@@ -0,0 +1,88 @@
+const User = require('../models/user.js')
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const utils = require('./utils')
+
+
+module.exports = function(app) {
+    // Register page
+    app.get('/register', (req, res) => {
+        let bodytype = utils.checklog("home", req.user)
+        res.render('register', {bodytype, user: req.user})
+    })
+
+    // Actual registry
+    app.post('/register', (req, res) => {
+        // Create User
+        let user = new User(req.body);
+        let username = user.username
+        let password = user.password
+        // Check if it's empty
+        if((!username) || (!password)){
+            return res.status(400).send('Cannot leave fields empty')
+            res.end
+        }
+
+        User.findOne({ 'username': username }).then((checkuser) => {
+            // Check if user already exists
+            if(checkuser){
+                return res.status(400).send('Username already exists')
+            }
+            else{
+            // Saving user and jwt token
+                user.save(() => {
+                    // Else, use jwt to make token and save to cookie, then redirect
+                    // NOTE: dotenv required
+                    let token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: "60 days" });
+                    res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                    res.redirect('/');
+                }).catch((err) => {
+                    res.send(err.message)
+                })
+            }
+        }).catch((err) => {
+            console.log(err, "Something happened while registering")
+        })
+
+
+    });
+
+    // Logout; clears cookies
+    app.get('/logout', (req, res) => {
+        res.clearCookie('nToken');
+        res.redirect('/');
+    });
+
+    // Login; render the login page
+    app.get('/login', (req, res) => {
+        let bodytype = utils.checklog("home", req.user)
+        res.render('login', {bodytype, user: req.user});
+    });
+
+    // LOGIN
+    app.post('/login', (req, res) => {
+        // Find a user that matches username
+        User.findOne({ username: req.body.username }, "+password")
+        .then((user) => {
+            // If a user does not exist, return error
+            if(!user){
+                return res.status(401).send({ message: 'Wrong email or password' });
+            }
+            // Compares password
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                // If password does not match, return error
+                if(!isMatch){
+                    return res.status(401).send({ message: 'Wrong email or password' });
+                }
+                // If it matches, make a cookie
+                let token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: "60 days" });
+                res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                res.redirect('/');
+            })
+        }).catch((err) => {
+            res.send(err.message)
+        })
+    });
+
+
+}
